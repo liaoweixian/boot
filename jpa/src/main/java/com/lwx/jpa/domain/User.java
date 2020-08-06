b04d506f4855630d21857d20852f5474251b5115
liaoweixian <LIao980526>
2020-08-07 00:17:55 +0800

    jpa 事例

--- jpa/src/main/java/com/lwx/jpa/domain/User.java
+++ jpa/src/main/java/com/lwx/jpa/domain/User.java
@@ -14,6 +14,14 @@ import java.util.Date;
 public class User implements Serializable {
 
     @Id
+    /**
+     * 用于标注主键的生成策略
+     * 组件的增长方式：
+     * IDENTITY 主键自增长 采用数据库ID自增长的方式来自增主键字断 Oracle不支持这种方式
+     * AUTO JPA自动选择合适的策略，是默认选秀
+     * SEQUENCE 通过序列化产生主键，通过@SequenceGenerator 注解指定序列名，MySql不支持这种方式
+     * TABLE 通过表产生主键，
+     */
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
