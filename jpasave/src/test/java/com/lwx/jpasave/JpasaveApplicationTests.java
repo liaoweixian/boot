ddf91ff772f630cef58da17fb6f4111c1b6e9cd9
liaoweixian <965932944@qq.com>
2020-08-12 19:00:18 +0800

    修改

---
+++ jpasave/src/test/java/com/lwx/jpasave/JpasaveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lwx.jpasave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpasaveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
