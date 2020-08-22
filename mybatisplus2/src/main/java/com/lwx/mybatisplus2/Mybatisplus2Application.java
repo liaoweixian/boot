5f34de1f96fde154551f3bb1c40b8d67830b8972
liaoweixian <LIao980526>
2020-08-22 22:31:04 +0800

    文件提交

---
+++ mybatisplus2/src/main/java/com/lwx/mybatisplus2/Mybatisplus2Application.java
@@ -0,0 +1,13 @@
+package com.lwx.mybatisplus2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mybatisplus2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mybatisplus2Application.class, args);
+    }
+
+}
---
+++ mybatisplus2/src/test/java/com/lwx/mybatisplus2/Mybatisplus2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lwx.mybatisplus2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mybatisplus2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
